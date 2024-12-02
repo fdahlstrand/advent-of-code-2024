12df17dc55aa0969a81d97efb1b129c4bce8822c
Fredrik Dahlstrand <32812964+fdahlstrand@users.noreply.github.com>
2024-12-02 19:24:14 +0100

    Refactor is_safe evaluation in day 2 puzzle

--- day02/day02.ml
+++ day02/day02.ml
@@ -20,20 +20,20 @@ let rec dampen d p lst =
 let is_in_range x = (1 <= x && x <= 3) || (-3 <= x && x <= -1)
 
 let is_safe report =
-  ( List.for_all (fun delta -> delta > 0) report
-  || List.for_all (fun delta -> delta < 0) report )
-  && List.for_all is_in_range report
+  let diff = difference report in
+  ( List.for_all (fun delta -> delta > 0) diff
+  || List.for_all (fun delta -> delta < 0) diff )
+  && List.for_all is_in_range diff
 
-let is_safe_dampened report =
-  dampen [report] [] report |> List.map difference |> List.exists is_safe
+let is_safe_dampened report = dampen [report] [] report |> List.exists is_safe
+
+let count_safe f reports = List.filter f reports |> List.length
 
 let reports = read_reports input_path
 
-let nbr_of_safe_reports =
-  List.map difference reports |> List.filter is_safe |> List.length
+let nbr_of_safe_reports = count_safe is_safe reports
 
-let nbr_of_safe_dampened_reports =
-  List.filter is_safe_dampened reports |> List.length
+let nbr_of_safe_dampened_reports = count_safe is_safe_dampened reports
 
 let () =
   Printf.printf
