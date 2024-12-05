408b88203c1dffb3cb7d012877be1a89d9224c02
Fredrik Dahlstrand <32812964+fdahlstrand@users.noreply.github.com>
2024-12-05 19:37:32 +0100

    Solve day 5, part 2

--- day05/day05.ml
+++ day05/day05.ml
@@ -1,4 +1,4 @@
-let input_path = "./input/day05/sample.txt"
+let input_path = "./input/day05/input.txt"
 
 let parse_rules inp =
   let parse_rule r =
@@ -45,6 +45,45 @@ let mid lst =
   let n = List.length lst in
   List.nth lst (((1 + n) / 2) - 1)
 
+let rec find_failed_rules rs n = function
+  | h :: t ->
+      if List.mem (n, h) rs then find_failed_rules rs n t
+      else (n, h) :: find_failed_rules rs n t
+  | _ ->
+      []
+
+let rec find_errors rs = function
+  | h :: t ->
+      find_failed_rules rs h t :: find_errors rs t
+  | _ ->
+      []
+
+let swap a b lst =
+  let rec swap' pre a b = function
+    | h :: t ->
+        if h = a then swap' (pre @ [b]) a b t
+        else if h = b then swap' (pre @ [a]) a b t
+        else swap' (pre @ [h]) a b t
+    | [] ->
+        pre
+  in
+  swap' [] a b lst
+
+let rec fix_update lst = function
+  | (a, b) :: t ->
+      fix_update (swap a b lst) t
+  | [] ->
+      lst
+
+let rec fix_all rs lst =
+  match lst with
+  | h :: t ->
+      let e = find_errors rs h |> List.flatten in
+      if List.is_empty e then h :: fix_all rs t
+      else fix_all rs (fix_update h e :: t)
+  | [] ->
+      []
+
 let correctly_ordered_sum =
   List.filter
     (fun us -> check_update rules us |> List.for_all (fun x -> x))
@@ -52,4 +91,14 @@ let correctly_ordered_sum =
   |> List.map (fun us -> mid us)
   |> List.fold_left ( + ) 0
 
-let () = Printf.printf "\nCorrectly ordered sum: %d\n" correctly_ordered_sum
+let incorrectly_ordered_sum =
+  List.filter
+    (fun us -> check_update rules us |> List.exists (fun x -> not x))
+    updates
+  |> fix_all rules
+  |> List.map (fun us -> mid us)
+  |> List.fold_left ( + ) 0
+
+let () =
+  Printf.printf "\nCorrectly ordered sum: %d\nIncorrectly ordered sum: %d\n"
+    correctly_ordered_sum incorrectly_ordered_sum
