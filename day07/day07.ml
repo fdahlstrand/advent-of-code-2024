624f3f1d048bc43208d0f077bbee37c6c422d31d
Fredrik Dahlstrand <32812964+fdahlstrand@users.noreply.github.com>
2024-12-07 13:24:19 +0100

    Solve day 7, part 2

--- day07/day07.ml
+++ day07/day07.ml
@@ -1,12 +1,16 @@
-let eval lst =
-  let rec eval' acc = function
+let eval ops lst =
+  let rec eval' ops acc = function
     | h :: t ->
-        if List.is_empty acc then eval' [h] t
-        else eval' (List.map (( + ) h) acc @ List.map (( * ) h) acc) t
+        if List.is_empty acc then eval' ops [h] t
+        else
+          eval' ops
+            (List.fold_left ( @ ) []
+               (List.map (fun op -> List.map (op h) acc) ops) )
+            t
     | [] ->
         acc
   in
-  eval' [] lst
+  eval' ops [] lst
 
 let read_equations path =
   let parse_numbers s =
@@ -22,13 +26,22 @@ let read_equations path =
   In_channel.with_open_text path In_channel.input_lines
   |> List.map parse_equation
 
-let is_eqn_possible (v, ns) = List.mem v (eval ns)
+let is_eqn_possible ops (v, ns) = List.mem v (eval ops ns)
+
+let calculate ops equations =
+  equations
+  |> List.filter (is_eqn_possible ops)
+  |> List.map fst |> List.fold_left ( + ) 0
+
+let ( ||^ ) a b = int_of_string (string_of_int b ^ string_of_int a)
 
 let equations = read_equations "./input/day07/input.txt"
 
-let calibration_result =
-  equations
-  |> List.filter is_eqn_possible
-  |> List.map fst |> List.fold_left ( + ) 0
+let calibration_result = calculate [( + ); ( * )] equations
 
-let () = Printf.printf "\nTotal calibration result: %d\n" calibration_result
+let calibration_result_2 = calculate [( + ); ( * ); ( ||^ )] equations
+
+let () =
+  Printf.printf
+    "\nTotal calibration result 1: %d\nTotal calibration result 2: %d\n"
+    calibration_result calibration_result_2
