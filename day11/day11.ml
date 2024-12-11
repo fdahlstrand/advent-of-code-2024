233e7d13f1e3dd73fe59054f1c8432b21b106d74
Fredrik Dahlstrand <32812964+fdahlstrand@users.noreply.github.com>
2024-12-11 19:36:07 +0100

    Solve day 11, part 2

--- day11/day11.ml
+++ day11/day11.ml
@@ -9,14 +9,39 @@ let process_stone s =
   else split_stone s
 
 let rec process_stones_aux n stones =
-  let () = Printf.printf "%d\n%!" n in
   if n = 0 then stones
   else process_stones_aux (n - 1) (List.map process_stone stones |> List.concat)
+
+let memo_predict_stone n s =
+  let cache = Hashtbl.create 10 in
+  let rec predict_stone n s =
+    try Hashtbl.find cache (n, s)
+    with Not_found ->
+      if n = 0 then 1
+      else
+        let next = process_stone s in
+        let cnt =
+          match next with
+          | [a; b] ->
+              predict_stone (n - 1) a + predict_stone (n - 1) b
+          | [a] ->
+              predict_stone (n - 1) a
+          | _ ->
+              failwith "This should not happen"
+        in
+        Hashtbl.add cache (n, s) cnt ;
+        cnt
+  in
+  predict_stone n s
 
 let stones =
   In_channel.with_open_text "./input/day11/input.txt" In_channel.input_all
   |> String.trim |> String.split_on_char ' '
 
 let () =
-  Printf.printf "\nNumber of stones: %d\n"
+  Printf.printf
+    "\n\
+     Number of stones after 25 blinks: %d\n\
+     Number of stones after 75 blinks: %d\n"
     (process_stones_aux 25 stones |> List.length)
+    (List.map (memo_predict_stone 75) stones |> List.fold_left ( + ) 0)
