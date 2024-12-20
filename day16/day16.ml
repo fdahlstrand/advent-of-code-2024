c94abdca6a04f6ceeb00460996f335b6d44dbe9c
Fredrik Dahlstrand <32812964+fdahlstrand@users.noreply.github.com>
2024-12-20 20:23:46 +0100

    [WIP] Partial all shortest path (dijkstra) for day 16

--- day16/day16.ml
+++ day16/day16.ml
@@ -1,3 +1,44 @@
+module PrioQueue = struct
+  type prio = int
+
+  type 'a queue = Empty | Node of prio * 'a * 'a queue * 'a queue
+
+  let empty = Empty
+
+  exception Queue_empty
+
+  let rec insert prio elt queue =
+    match queue with
+    | Empty ->
+        Node (prio, elt, Empty, Empty)
+    | Node (p, e, left, right) ->
+        if prio <= p then Node (prio, elt, insert p e right, left)
+        else Node (p, e, insert prio elt right, left)
+
+  let rec remove_top = function
+    | Empty ->
+        raise Queue_empty
+    | Node (_, _, left, Empty) ->
+        left
+    | Node (_, _, Empty, right) ->
+        right
+    | Node
+        ( _
+        , _
+        , (Node (lprio, lelt, _, _) as left)
+        , (Node (rprio, relt, _, _) as right) ) ->
+        if lprio <= rprio then Node (lprio, lelt, remove_top left, right)
+        else Node (rprio, relt, left, remove_top right)
+
+  let extract_min = function
+    | Empty ->
+        raise Queue_empty
+    | Node (prio, elt, _, _) as queue ->
+        (prio, elt, remove_top queue)
+
+  let is_empty = function Empty -> true | _ -> false
+end
+
 type map_element = Empty | Wall | Start | End
 
 type direction = North | East | South | West
@@ -10,7 +51,15 @@ module Pos = struct
     else Stdlib.compare r1 r2
 end
 
+module Node = struct
+  type t = (int * int) * direction
+
+  let compare = Stdlib.compare
+end
+
 module Maze = Map.Make (Pos)
+module NodeMap = Map.Make (Node)
+module NodeSet = Set.Make (Node)
 
 let read_map path =
   In_channel.with_open_text path In_channel.input_lines
@@ -146,6 +195,44 @@ let walk maze =
         Maze.find end_pos visited
   in
   walk' maze [(start_pos, East, 0)] Maze.empty
+
+let update_neighbours maze q seen ((pos_u, dir_u), dist_u) dist prev =
+  let ns =
+    next_moves maze (pos_u, dir_u, dist_u)
+    |> List.filter (fun (pos, dir, _) -> not (NodeSet.mem (pos, dir) seen))
+  in
+  List.fold_left
+    (fun (q', dist', prev') (pos, dir, alt) ->
+      let dist_v =
+        NodeMap.find_opt (pos, dir) dist' |> Option.value ~default:Int.max_int
+      in
+      (*TODO: Handle case when alt = dist_v *)
+      if alt < dist_v then
+        ( q' (*TODO: Add v to q*)
+        , NodeMap.update (pos, dir) (fun _ -> Some alt) dist'
+        , NodeMap.update (pos, dir) (fun _ -> Some [(pos_u, dir_u)]) prev' )
+      else (q', dist', prev') )
+    (q, dist, prev) ns
+
+let dijkstra maze =
+  let start_pos = find_start maze in
+  let end_pos = find_end maze in
+  let rec dijkstra_aux q seen dist prev =
+    if PrioQueue.is_empty q then (dist, prev)
+    else
+      let dist_u, u, q' = PrioQueue.extract_min q in
+      (* Don't break when u = end_pos. There might be more paths that are equally good *)
+      let q', dist', prev' =
+        update_neighbours maze q' seen (u, dist_u) dist prev
+      in
+      let seen' = NodeSet.add u seen in
+      dijkstra_aux q' seen' dist' prev'
+  in
+  dijkstra_aux
+    PrioQueue.(empty |> insert 0 (start_pos, East))
+    NodeSet.empty
+    NodeMap.(empty |> add (start_pos, East) 0)
+    NodeMap.empty
 
 let () =
   Printf.printf "\nLowest score: %d\n%!"
